a68ab48c8c4ed44b651966a8a739dd95f346b69f
cjhodnefield <cjhodnefield@gmail.com>
2016-05-18 14:18:16 -0500

    Request updated

--- Request.java
+++ Request.java
@@ -4,11 +4,18 @@ class Request {
 	// Path and opcode variables
 	private String path;
 	private String method;
+	private boolean keepAlive;
 
 	// Constructor
 	Request(ArrayList<ArrayList<String>> lines) {
 		this.method = lines.get(0).get(0);
 		this.path = lines.get(0).get(1);
+		String keepAliveStr = lines.get(2).get(1);
+		if (keepAliveStr.equals("keep-alive")) {
+			this.keepAlive = true;
+		} else {
+			this.keepAlive = false;
+		}
 	}
 
 	// Methods
@@ -17,8 +24,11 @@ class Request {
 		return this.method;
 	}
 
-
 	public String getPath(){
 		return this.path; 
+	}
+	
+	public boolean keepAlive() {
+		return this.keepAlive;
 	}
 }
